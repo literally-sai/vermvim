f889279560551e3524fe2afd447e34777c478612
literally-sai <sai@literally-sai.com>
2025-12-15 22:08:57 +0100

    Add two marathon inspired themes

---
+++ config/colors/themes/marathon-light.nix
@@ -0,0 +1,18 @@
+{
+  base00 = "#e4ebef";
+  base01 = "#b7bec9";
+  base02 = "#a6abb3";
+  base03 = "#6d7a8b";
+  base04 = "#585c83";
+  base05 = "#303d50";
+  base06 = "#cbfe00";
+  base07 = "#0d1414";
+  base08 = "#660a0f";
+  base09 = "#662b00";
+  base0A = "#526600";
+  base0B = "#326610";
+  base0C = "#006666";
+  base0D = "#003366";
+  base0E = "#400066";
+  base0F = "#660040";
+}
---
+++ config/colors/themes/marathon.nix
@@ -0,0 +1,18 @@
+{
+  base00 = "#0D1414";
+  base01 = "#2A2C30";
+  base02 = "#303D50";
+  base03 = "#585C83";
+  base04 = "#6D7A8B";
+  base05 = "#A6ABB3";
+  base06 = "#B7BEC9";
+  base07 = "#E4EBEF";
+  base08 = "#BFE8F8";
+  base09 = "#FE0104";
+  base0A = "#B6CF62";
+  base0B = "#C0F71E";
+  base0C = "#7BCDD2";
+  base0D = "#3A87E5";
+  base0E = "#77A4D9";
+  base0F = "#72B5DC";
+}
