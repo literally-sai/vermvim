18925f25d93c4ee4607869ea64cf838256a0f96e
literally-sai <sai@literally-sai.com>
2025-12-16 12:12:12 +0100

    Enable autoArchive for nil_ls

--- config/plugins/lsp.nix
+++ config/plugins/lsp.nix
@@ -10,6 +10,7 @@
       ts_ls.enable = true;
       clangd.enable = true;
       nil_ls.enable = true;
+      nil_ls.settings.nix.flake.autoArchive = true;
       cssls.enable = true;
       jsonls.enable = true;
       dockerls.enable = true;
