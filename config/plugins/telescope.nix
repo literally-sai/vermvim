a00b21b790a70ce995af1d63363fb81b7507cffd
literally-sai <sai@literally-sai.com>
2025-12-21 17:42:54 +0100

    Enable hidden files search

--- config/plugins/telescope.nix
+++ config/plugins/telescope.nix
@@ -2,5 +2,22 @@
   plugins.telescope = {
     enable = true;
     extensions.fzf-native.enable = true;
+    settings = {
+      pickers = {
+        find_files = {
+          hidden = true;
+        };
+        live_grep = {
+          additional_args = [ "--hidden" ];
+        };
+      };
+      defaults = {
+        file_ignore_patterns = [
+          ".git/"
+          "node_modules"
+          "venv"
+        ];
+      };
+    };
   };
 }
