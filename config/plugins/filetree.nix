459b23d2ee4412f65ade37f3e9e48e0d2dc8b23d
literally-sai <sai@literally-sai.com>
2025-12-21 18:21:28 +0100

    Place view right

--- config/plugins/filetree.nix
+++ config/plugins/filetree.nix
@@ -4,6 +4,7 @@
     settings = {
       sync_root_with_cwd = true;
       respect_buf_cwd = true;
+      view.side = "right";
       update_focused_file = {
         enable = true;
         update_root = true;
