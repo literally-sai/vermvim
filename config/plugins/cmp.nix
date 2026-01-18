c7a829a98d9ecb0c901b622131490845dcc3cc24
literally-sai <sai@literally-sai.com>
2026-01-18 05:50:54 +0100

    Fix jump

--- config/plugins/cmp.nix
+++ config/plugins/cmp.nix
@@ -79,6 +79,36 @@
           "<C-n>" = "cmp.mapping.select_next_item()";
           "<C-u>" = "cmp.mapping.scroll_docs(-4)";
           "<C-d>" = "cmp.mapping.scroll_docs(4)";
+          "<Tab>" = {
+            __raw = ''
+              cmp.mapping(function(fallback)
+                local luasnip = require("luasnip")
+                if cmp.visible() then
+                  cmp.select_next_item()
+                elseif luasnip.expand_or_jumpable() then
+                  luasnip.expand_or_jump()
+                elseif has_words_before() then
+                  cmp.complete()
+                else
+                  fallback()
+                end
+              end, { "i", "s" })
+            '';
+          };
+          "<S-Tab>" = {
+            __raw = ''
+              cmp.mapping(function(fallback)
+                local luasnip = require("luasnip")
+                if cmp.visible() then
+                  cmp.select_prev_item()
+                elseif luasnip.jumpable(-1) then
+                  luasnip.jump(-1)
+                else
+                  fallback()
+                end
+              end, { "i", "s" })
+            '';
+          };
         };
       };
     };
